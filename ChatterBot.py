9cc3f73ecbca63b5b0e7993b4de1ea5479cadda0
Jayleh <jaylehyun@gmail.com>
2018-03-06 21:16:52 -0800

    erased keys

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "ec6gL3ASXa7jBNNmb3tuyVsJM"
-consumer_secret = "bOtaxqvBur8bVxiSLf6qkFXr1QXqmQZGP664c3iIOOB8EweWSk"
-access_token = "969399323145289729-E6sNmYKi96fMPFBKs7BmynG9VQ2LoN4"
-access_token_secret = "4YsEHlScIp9YNb0IrEkvT7fJemTGYjnnPgoVW8fpJF4g9"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
