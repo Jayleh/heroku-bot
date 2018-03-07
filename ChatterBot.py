0871eb03816505ce868e61fa327b127d8ac7d02f
Jayleh <jaylehyun@gmail.com>
2018-03-06 21:00:03 -0800

    changing keys

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,10 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = "ec6gL3ASXa7jBNNmb3tuyVsJM"
+consumer_secret = "bOtaxqvBur8bVxiSLf6qkFXr1QXqmQZGP664c3iIOOB8EweWSk"
+access_token = "969399323145289729-E6sNmYKi96fMPFBKs7BmynG9VQ2LoN4"
+access_token_secret = "4YsEHlScIp9YNb0IrEkvT7fJemTGYjnnPgoVW8fpJF4g9"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
